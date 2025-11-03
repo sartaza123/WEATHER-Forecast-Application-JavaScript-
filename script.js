46ff18f2680382c749f5ee4826adb5f9a5f7e8b7
sartaz <sartaza123@gmail.com>
2025-11-03 19:14:09 +0530

    script added to inner htmle for current temprature and weather also validate the input feel can't be empty

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const searchBtn = document.querySelector("#search");
-const input = document.querySelector("#input");
+let input = document.querySelector("#input");
 const tempratureDisplay = document.querySelector(".temprature");
 
 searchBtn.addEventListener("click", () => {
+  let isCelsioous = true;
+
   async function getWeather() {
     const city = input.value.trim();
     if (city === "") {
-      console.log("please inser city name");
+      console.log("please enter city name");
       return;
     }
     const url = `https://api.weatherapi.com/v1/current.json?key=02193e91dd7b4b849d4104126250211&q=${city}&aqi=yes`;
@@ -14,35 +16,78 @@ searchBtn.addEventListener("click", () => {
     try {
       const response = await fetch(url);
       const result = await response.json();
-      // console.log(result);
 
-      const tempC = result.current.temp_c;
-      const tempF = result.current.temp_f;
+      // searched location ==============================================
       const location = result.location.name;
+      const locationCards = document.querySelector(".recent-location-cards");
+      locationCards.innerHTML = `
+      <div class="locations px-3 py-1 mr-6 flex liquid-glass">
+        <div class="location-logo mr-1">
+          <ion-icon name="location-outline"></ion-icon>
+        </div>
+        <div class="location-name">${location}</div>
+      </div>`;
+
+      // temprature =====================================================
+      const temp_c = parseInt(result.current.temp_c);
+      const temp_f = parseInt(result.current.temp_f);
+      tempratureDisplay.innerHTML = `
+          ${temp_c}<ion-icon name="radio-button-off-outline"
+            class="text-xl mb-9 ml-0.5"></ion-icon>C`;
+
+      // weather report =================================================
       const weather = result.current.condition.text;
-      const feelslike_c = result.current.feelslike_c;
-      const feelslike_f = result.current.feelslike_f;
+      const weatherReport = (document.querySelector(
+        ".weather-report"
+      ).innerHTML = weather);
+
+      // feels like =====================================================
+      const feelslike_c = parseInt(result.current.feelslike_c);
+      const feelslike_f = parseInt(result.current.feelslike_f);
+      const feelsC = document.querySelector(".feels-like");
+      feelsC.innerHTML = `Feels Like ${feelslike_c}<ion-icon
+        name="radio-button-off-outline"
+        class="text-[6px] mb-2"></ion-icon>C`;
+
+      // current weather toggle between Cel & Fe ========================
+
+      const weather_cel_fer = document.querySelector(".current-weather");
+      weather_cel_fer.addEventListener("click", () => {
+        if (isCelsioous === false) {
+          // temprature in Celcious =================
+          tempratureDisplay.innerHTML = `
+          ${temp_c}<ion-icon name="radio-button-off-outline" class="text-xl mb-9 ml-0.5"></ion-icon>C`;
+
+          // temprature in fahrenhite ================
+          feelsC.innerHTML = `Feels Like ${feelslike_c}<ion-icon
+          name="radio-button-off-outline"
+          class="text-[6px] mb-2"></ion-icon>C`;
+        } else {
+          // feelsLike in Celcious =================
+          tempratureDisplay.innerHTML = `
+          ${temp_f}<ion-icon name="radio-button-off-outline"
+          class="text-xl mb-9 ml-0.5"></ion-icon>F`;
+
+          // feelsLike in fahrenhite ================
+          feelsC.innerHTML = `Feels Like ${feelslike_f}<ion-icon
+            name="radio-button-off-outline"
+            class="text-[6px] mb-2"></ion-icon>F`;
+        }
+        isCelsioous = !isCelsioous;
+      });
+
       const humidity = result.current.humidity;
       const wind_kph = result.current.wind_kph;
       const wind_mph = result.current.wind_mph;
-      console.log(
-        tempC,
-        tempF,
-        location,
-        weather,
-        feelslike_c,
-        feelslike_f,
-        humidity,
-        wind_kph,
-        wind_mph
-      );
-      console.log(result.current);
+      // console.log(result.current);
+      input.value = "";
     } catch (error) {
       console.error(error);
     }
   }
   getWeather();
 });
+input.value = "";
 
 // api key
 // 02193e91dd7b4b849d4104126250211
